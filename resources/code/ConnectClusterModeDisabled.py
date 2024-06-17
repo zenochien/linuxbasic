648aee6c5469258fb706cc44b85601e6c2b7d1ec
zenochien <seungholove5@gmail.com>
2024-06-17 16:38:59 +0700

    check images

--- resources/code/ConnectClusterModeDisabled.py
+++
@@ -1,8 +0,0 @@
-from redis import Redis
-import logging
-
-logging.basicConfig(level=logging.INFO)
-redis = Redis(host='primary.xxx.yyyyyy.zzz1.cache.amazonaws.com', port=6379, decode_responses=True, ssl=True, username='myuser', password='MyPassword0123456789')
-
-if redis.ping():
-    logging.info("Connected to Redis")
--- resources/code/ConnectClusterModeEnabled.py
+++
@@ -1,8 +0,0 @@
-from rediscluster import RedisCluster
-import logging
-
-logging.basicConfig(level=logging.INFO)
-redis = RedisCluster(startup_nodes=[{"host": "xxx.yyy.clustercfg.zzz1.cache.amazonaws.com","port": "6379"}], decode_responses=True,skip_full_coverage_check=True)
-
-if redis.ping():
-    logging.info("Connected to Redis")
